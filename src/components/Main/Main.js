809ad45dadb8896c56a44e3da275622594d3dbec
mira-kine <mirak0419@gmail.com>
2021-11-30 11:48:43 -0800

    added CSS to footer header main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Main.css'
 import { zodiac } from '../../data'
 import ZodiacCard from '../Zodiacs/ZodiacCard'
+import background from '../../background.png'
 
 export default function Main() {
   return (
-    <main className="zodiac-signs">
+    <main className="background" style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map((item) => (
         <ZodiacCard key={item.name} {...item} />
       ))}
--- src/components/Zodiacs/ZodiacCard.js
+++ src/components/Zodiacs/ZodiacCard.js
@@ -3,7 +3,7 @@ import './ZodiacCard.css'
 
 export default function ZodiacCard({ name, dates }) {
   return (
-    <div className="zodiacs">
+    <div className="zodiac-card">
       <img alt={name} src={`${process.env.PUBLIC_URL}/zodiacs/${name}.png`}></img>
       <div className="name">{name}</div>
       <div>{dates}</div>
